b3f00761aba13b1c146bdc1bc0bc71cfaf3d284c
juandavid_dev <juandavid.dev@gmail.com>
2023-12-17 10:31:21 -0500

    V.0.2
    Se agrega menu desplegable para mujer y hombre

--- src/components/molecules/Nav.jsx
+++ src/components/molecules/Nav.jsx
@@ -1,19 +1,56 @@
+import { useState } from 'react';
 import '/src/components/molecules/nav.scss'
 
 import MoviesIconoNav from '/src/img/movies-icono-nav.svg'
 
 const Nav = () => {
+
+    const [showDropdown, setShowDropdown] = useState(false)
+
+    const handleMouseEnter = () => {
+        setShowDropdown(true)
+    }
+
+    const handleMouseLeave = () => {
+        setShowDropdown(false)
+    }
+
     return (
         <div className='navbar-nav'>
             <ul className='nav-links'>
-                <li>
+                <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                     <span>ㅤ</span>
                     <a href='#' target='_blank' className='links-link'>Mujer</a>
+                    {showDropdown && (
+                        <div className='dropdown-content'>
+                            <p>MUJER</p>
+                            <a href='#' target='_blank'>Camisas</a>
+                            <a href='#' target='_blank'>Camisetas</a>
+                            <a href='#' target='_blank'>Pijamas</a>
+                            <a href='#' target='_blank'>Ropa interior</a>
+                            <a href='#' target='_blank'>Chaquetas y buzos</a>
+                            <a href='#' target='_blank'>Joggers, pantalones y shorts</a>
+                            <a href='#' target='_blank'>Calzado</a>
+                            <a href='#' target='_blank'>Accesorios</a>
+                        </div>
+                    )}
                     <span>ㅤ</span>
                 </li>
-                <li>
+                <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                     <span>ㅤ</span>
                     <a href='#' target='_blank' className='links-link'>Hombre</a>
+                    {showDropdown && (
+                        <div className='dropdown-content'>
+                            <p>HOMBRE</p>
+                            <a href='#' target='_blank'>Camisas</a>
+                            <a href='#' target='_blank'>Camisetas</a>
+                            <a href='#' target='_blank'>Pijamas y ropa interior</a>
+                            <a href='#' target='_blank'>Chaquetas y buzos</a>
+                            <a href='#' target='_blank'>Joggers, pantalones y bermudas</a>
+                            <a href='#' target='_blank'>Accesorios</a>
+                            <a href='#' target='_blank'>Pantalonetas de baño</a>
+                        </div>
+                    )}
                     <span>ㅤ</span>
                 </li>
                 <li>
