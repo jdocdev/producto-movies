9cdfef4ccfce7cf295041763cd483973af608f5d
juandavid_dev <juandavid.dev@gmail.com>
2023-12-17 11:31:06 -0500

    V.0.3
    Se agrega menu desplegable para mobile

--- src/components/molecules/NavMobile.jsx
+++ src/components/molecules/NavMobile.jsx
@@ -1,10 +1,26 @@
 import '/src/components/molecules/navMobile.scss'
+import { useState } from 'react'
 
 const NavMobile = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
-    <div className='container-navMobile'>
-        <a><i className="bi bi-justify-left"></i></a>
-        <a><i className="bi bi-search"></i></a>
+    <div className={`container-navMobile ${menuOpen ? 'open' : ''}`}>
+      <a onClick={toggleMenu}><i className="bi bi-justify-left"></i></a>
+      <a><i className="bi bi-search"></i></a>
+      {menuOpen && (
+        <div className="menu-list">
+          <a href="#" target='_blank'>MUJER</a>
+          <a href="#" target='_blank'>HOMBRE</a>
+          <a href="#" target='_blank'>NUEVOS <span>LANZAMIENTOS</span></a>
+          <a href="#" target='_blank'>OFERTAS <span>DE TEMPORADA</span></a>
+        </div>
+      )}
     </div>
   )
 }
